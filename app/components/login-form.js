ffcc699c7022ed7007750631e00552487aabb235
joannecas <joanne@showbie.com>
2021-11-23 08:09:39 -0700

    component integration test

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -1,7 +1,36 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class LoginFormComponent extends Component {
- onLoginFormSubmit(evt) {
-     debugger;
- }
+  @tracked
+  userId = null;
+
+  get isDisabled() {
+      return !this.userId;
+  }
+
+  loginAsUserWithId(val) {
+    console.log(val);
+  }
+
+  /**
+   *
+   * @param {Event & {target: HTMLFormElement}} evt
+   */
+  @action
+  onLoginFormSubmit(evt) {
+    evt.preventDefault();
+    const { target } = evt;
+    const val = target.querySelector('select').value;
+    this.loginAsUserWithId(val);
+  }
+
+  /**
+   * @param {Event & { target: HTMLSelectElement }} evt
+   */
+  @action
+  onSelectChanged(evt) {
+      this.userId = evt.target.value;
+  }
 }
